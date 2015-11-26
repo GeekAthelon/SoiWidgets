f054e91447567dc0475e9bb043ba9e22a8386446
Athelon DaGeek <GeekAthelon@gmail.com>
2015-11-26 00:48:28 -0500

    Hashed the usernames so it no longer leaks back private information.

--- rooms/BlameTheCards/code/server/code/client/btc-client.js
+++ rooms/BlameTheCards/code/server/code/client/btc-client.js
@@ -6,10 +6,38 @@ var gameUrl = 'http://76.188.44.119:1701';
 //};
 
 window.onload = function() {
-    const username = document.getElementsByName('vqxus')[0].value.toLowerCase();
+    const soiUsername = document.getElementsByName('vqxus')[0].value.toLowerCase();
+    const username = hashFnv32a(soiUsername, true);
     const playerAnswers = [];
     let gameState;
     let timerId;
+
+    /**
+     * Calculate a 32 bit FNV-1a hash
+     * Found here: https://gist.github.com/vaiorabbit/5657561
+     * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+     *
+     * @param {string} str the input value
+     * @param {boolean} [asString=false] set to true to return the hash value as
+     *     8-digit hex string instead of an integer
+     * @param {integer} [seed] optionally pass the hash of the previous chunk
+     * @returns {integer | string}
+     */
+    function hashFnv32a(str, asString, seed) {
+        /*jshint bitwise:false */
+        var i, l,
+            hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+        for (i = 0, l = str.length; i < l; i++) {
+            hval ^= str.charCodeAt(i);
+            hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+        }
+        if (asString) {
+            // Convert to 8 digit hex string
+            return ('0000000' + (hval >>> 0).toString(16)).substr(-8);
+        }
+        return hval >>> 0;
+    }
 
     function setTimer(countDown) {
         clearTimeout(timerId);
