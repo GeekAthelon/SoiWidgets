376a53b3ad59d497b47a71dd6eceb71ae90165a7
Athelon DaGeek <GeekAthelon@gmail.com>
2015-12-16 03:20:08 -0500

    Database paths now created properly and available in both unit testing and regular mode.

--- rooms/BlameTheCards/code/gulp.config.js
+++ rooms/BlameTheCards/code/gulp.config.js
@@ -1,19 +1,10 @@
 module.exports = function() {
-	var config = {
-	};
-
-	config.src = ['./server/code/**/*.js'];
-	config.dest = './build';
-	config.srcDir = './server/code';
-
-	config.nodeServer = './server/code/app/btc.js';
-	
-	config.prod = {
-		port: 5000,
-		url: "http://76.188.44.119:5000"
-	};
-	
-
-	
-	return config;
+  var config = {
+    src: ['./server/code/**/*.js'],
+    dest: './build',
+    srcDir: './server/code',
+    nodeServer: './server/code/app/btc.js'
+  };
+  
+  return config;
 }
--- rooms/BlameTheCards/code/server/code/app/get-btc-config.js
+++ rooms/BlameTheCards/code/server/code/app/get-btc-config.js
@@ -2,7 +2,10 @@ function getBtcConfig() {
     'use strict';
 
     const nodeEnv = process.env.NODE_ENV;
+    const isTest = process.env.NODE_TEST;
+
     const fs = require('fs');
+    const path = require('path');
     const stringFormat = require('./lib/string-format');
     const configurationFile = './btc-config.json';
 
@@ -21,6 +24,15 @@ function getBtcConfig() {
         configuration.soi
     );
 
+    configuration.isTest = (isTest === 'true');
+    if (configuration.isTest) {
+        configuration.env.dbPath = configuration.env.dbPathTest;
+    } else {
+        configuration.env.dbPath = configuration.env.dbPathReal;
+    }
+
+    configuration.env.dbPath = path.resolve(configuration.env.dbPath);
+    console.log(configuration);
     return configuration;
 }
 
