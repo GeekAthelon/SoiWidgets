3634c8cd662f17667897c04170b0f5ce67f96937
Athelon DaGeek <GeekAthelon@gmail.com>
2015-12-16 12:58:54 -0500

    Code passes vetting.

--- rooms/BlameTheCards/code/server/code/app/lib/card-stack-manager.js
+++ rooms/BlameTheCards/code/server/code/app/lib/card-stack-manager.js
@@ -101,6 +101,12 @@ class CardStackManager {
     drawQuestion() {
         if (this.questionDrawStack._cards.length === 0) {
             btcBot.addMessage(`Shuffled Question Deck`);
+            console.log(
+                'Shuffling question deck: ',
+                this.questionDrawStack._cards.length,
+                this.questionDiscardStack._cards.length
+            );
+
             reshuffle(this.questionDiscardStack, this.questionDrawStack);
         }
         return this.questionDrawStack.draw();
@@ -109,6 +115,10 @@ class CardStackManager {
     drawAnswer() {
         if (this.answerDrawStack._cards.length === 0) {
             btcBot.addMessage(`Shuffled Answer Deck`);
+            console.log(
+                'Shuffling answer deck: ',
+                this.answerDrawStack._cards.length,
+                this.answerDiscardStack._cards.length);
             reshuffle(this.answerDiscardStack, this.answerDrawStack);
         }
         return this.answerDrawStack.draw();
