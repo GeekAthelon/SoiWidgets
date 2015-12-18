fe5777199f20d16f09b68fcd7d1dbfabbe6df58d
Athelon DaGeek <GeekAthelon@gmail.com>
2015-12-18 04:43:20 -0500

    Added lib and unit tests for register users.

---
+++ rooms/BlameTheCards/code/server/code/test/register-users-mocha.js
@@ -0,0 +1,58 @@
+/* globals it: true, describe: true, before: true */
+'use strict';
+
+const expect = require('chai').expect;
+const RegisterUsers = require('../app/lib/register-users.js')
+
+
+describe.only('Testing RegistereUser', () => {
+    let registerUsers;
+
+    before(() => {
+        console.log(RegisterUsers);
+        registerUsers = new RegisterUsers();
+        return registerUsers.clear();
+    });
+
+    it('Testing registereUsers instanceOf RegistereUsers', () => {
+        expect(registerUsers instanceof RegisterUsers).to.equal(true);
+    });
+
+    it('Adding a user', ()  =>{
+        return registerUsers.add('BotBoy').then(details => {
+            expect(details.token.length).to.equal(96);
+        });
+    });
+
+    it('Adding a user a second time should give a new token', () => {
+        let token1;
+        return registerUsers.add('BotBoy')
+            .then(details => {
+                token1 = details.token;
+                return registerUsers.add('BotBoy');
+            }).then(details => {
+                const token2 = details.token;
+                expect(token1).to.not.equal(token2);
+            });
+    });
+
+    it('Testing to make sure verify works.', () => {
+        return registerUsers.add('BotBoy')
+            .then(details => {
+                const token = details.token;
+                return registerUsers.verify('BotBoy', details.token);
+            }).then(isVerified => {
+                expect(isVerified).to.be.true;
+            });
+    });
+
+    it('Testing to make sure verify of a bad token fails.', () => {
+        return registerUsers.add('BotBoy')
+            .then(details => {
+                return registerUsers.verify('BotBoy', `Z${details.token}Z`);
+            }).then(isVerified => {
+                expect(isVerified).to.be.false;
+            });
+    });
+
+});
