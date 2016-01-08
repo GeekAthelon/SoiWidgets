6f8c6e69d71249bf064c79e2913d6e4c67203fe8
Athelon DaGeek <GeekAthelon@gmail.com>
2016-01-08 14:57:17 -0500

    BTC: Added grammar.tokenizer

--- rooms/BlameTheCards/code/server/code/client/lib/grammar-tool.js
+++ rooms/BlameTheCards/code/server/code/client/lib/grammar-tool.js
@@ -3,10 +3,87 @@
     'use strict';
 
     const gtool = (function() {
+        // This code is mostly English-centric. That's OK, since I'm only dealing in English.
+        // I don't mind checking for letters in the extended set.
+        // THIS CODE WILL BREAK WITH surrogate pairs
+
+        const TOKEN_WORD = 1000;
+        const TOKEN_OTHER = 2000;
+
+        const letters = 'ÀÈÌÒÙàèìòùÁÉÍÓÚÝáéíóúýÂÊÎÔÛâêîôûÃÑÕãñõÄËÏÖÜäëïöüçÇßØøÅåÆæÞþÐð' +
+            'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        function isLetter(ch) {
+            const r = letters.indexOf(ch) !== -1;
+            return r;
+        }
+
+        function tokenize(str) {
+            const l = str.length;
+            const tokens = [];
+            let i = 0;
+
+            function readWord() {
+                let s = '';
+                while (i < l) {
+                    s += str.charAt(i);
+
+                    if (isLetter(str.charAt(i + 1))) {
+                        i++;
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+                return s;
+            }
+
+            function readOther() {
+                let s = '';
+                while (i < l) {
+                    s += str.charAt(i);
+
+                    if (isLetter(str.charAt(i + 1))) {
+                        break;
+                    } else {
+                        i++;
+                        continue;
+                    }
+                }
+                return s;
+            }
+
+            while (i < l) {
+                const ch = str.charAt(i);
+                let s;
+                let type;
+
+                if (isLetter(ch)) {
+                    s = readWord();
+                    type = TOKEN_WORD;
+                } else {
+                    s = readOther();
+                    type = TOKEN_OTHER;
+                }
+                i++;
+
+                tokens.push({
+                    str: s,
+                    type: type
+                });
+            }
+
+            return tokens;
+        }
+
         return {
             isAlive: function() {
                 return true;
-            }
+            },
+            isLetter,
+            tokenize,
+            TOKEN_WORD,
+            TOKEN_OTHER
         };
     }());
 
--- rooms/BlameTheCards/code/server/code/test/client/lib/grammar-tool-mocha.js
+++ rooms/BlameTheCards/code/server/code/test/client/lib/grammar-tool-mocha.js
@@ -5,7 +5,7 @@
 const expect = require('chai').expect;
 const grammar = require('../../../client/lib/grammar-tool');
 
-describe('Grammer Tool Tests', function() {
+describe.only('Grammer Tool Tests', function() {
     before(function() {});
     beforeEach(function() {});
     afterEach(function() {});
@@ -14,4 +14,81 @@ describe('Grammer Tool Tests', function() {
         expect(grammar.isAlive()).to.deep.equal(true);
     });
 
+    it('Testing isLetter', () => {
+        expect(grammar.isLetter('a')).to.deep.equal(true);
+        expect(grammar.isLetter('Z')).to.deep.equal(true);
+        expect(grammar.isLetter('Ë')).to.deep.equal(true);
+        expect(grammar.isLetter('Ð')).to.deep.equal(true);
+
+        expect(grammar.isLetter(' ')).to.deep.equal(false);
+        expect(grammar.isLetter('-')).to.deep.equal(false);
+    });
+
+    it('Testing tokenize', () => {
+        let t1 = grammar.tokenize('Now "hear" this!  The quick  doggy --ran-- down the hall.');
+
+        let expected = [{
+            str: 'Now',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: ' "',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'hear',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: '" ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'this',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: '!  ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'The',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: ' ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'quick',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: '  ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'doggy',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: ' --',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'ran',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: '-- ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'down',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: ' ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'the',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: ' ',
+            type: grammar.TOKEN_OTHER
+        }, {
+            str: 'hall',
+            type: grammar.TOKEN_WORD
+        }, {
+            str: '.',
+            type: grammar.TOKEN_OTHER
+        }];
+
+        expect(t1).to.deep.equal(expected);
+    });
 });
