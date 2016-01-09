fe384aeb9a1494534a8dc5a818505921432385db
Athelon DaGeek <GeekAthelon@gmail.com>
2016-01-08 20:03:32 -0500

    BTC: Cards marked with grammar rules

--- rooms/BlameTheCards/code/server/code/app/lib/question-card.js
+++ rooms/BlameTheCards/code/server/code/app/lib/question-card.js
@@ -4,8 +4,20 @@ const Card = require('./card');
 const Deck = require('./deck');
 
 class QuestionCard extends Card {
-    constructor(num, text) {
+    constructor(num, fullText) {
         super();
+        this.rules = [];
+
+        let text = fullText;
+        while (text.indexOf('[') !== -1) {
+            const start = text.indexOf('[');
+            const end = text.indexOf(']');
+            const t = text.substring(start, end + 1);
+            const ruleText = t.replace('[', '').replace(']', '');
+
+            text = text.replace(t, '_');
+            this.rules = (ruleText.split(' '));
+        }
 
         this.num = num;
         this.text = text;
