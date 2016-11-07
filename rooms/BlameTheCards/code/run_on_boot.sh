74af5d976276141dd33affc22f6bab9c143249bf
GeekAthelon <GeekAthelon@gmail.com>
2016-11-07 01:17:06 -0500

    Fixed the 'run_on_boot' command
    Shouldn't really be part of the distro, but oh well.
    
    Its there.

--- rooms/BlameTheCards/code/run_on_boot.sh
+++ rooms/BlameTheCards/code/run_on_boot.sh
@@ -1,8 +1,26 @@
+
+if [ `id -u` -ne 0 ] ; then
+	echo "Please run as root" 
+ 	exit 1 
+fi
+
+echo Configuring the system.
+
+ddns --hostname geekathelon.daplie.me --agree --email geekathelon@gmail.com --token /usr/local/lib/node_modules/ddns-cli/tests/.daplie.me.jwt 
+
+cd ~athelon/lets_encrypt/letsencrypt
+./letsencrypt-auto renew
+cd ..
+
+cp /etc/letsencrypt/live/geekathelon.daplie.me/privkey.pem .
+cp /etc/letsencrypt/live/geekathelon.daplie.me/cert.pem .
+
+
+
+
 echo Running game
 
-cd ~/soi/SoiWidgets/rooms/BlameTheCards/code 
+cd ~athelon/soi/SoiWidgets/rooms/BlameTheCards/code
 
 # ./gulp build > /dev/null 2> /dev/null
-
-
-gulp serve > /dev/null 2> /dev/null &
+./node_modules/.bin/gulp serve
