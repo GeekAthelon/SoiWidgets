6326e0b9acbf1975ad674373e1cee2857e587e7a
Geek Athelon <geekathelon@gmail.com>
2020-05-24 00:08:14 -0400

    More work

--- rooms/flipit/flipit.js
+++ rooms/flipit/flipit.js
@@ -19,22 +19,23 @@ const CELL_BLACK_CLASS = 'cell_black';
 const POST_CLASS = "save-game";
 const CELL_CLASS = "flipit-cell";
 
-const calculateFlipped = (square, color) => {
-    const pos = squareToPosition(square);
+const calculateFlipped = (cellID, playerColor, enemyColor) => {
+    const pos = cellIDToPosition(cellID);
     const maxDim = Math.max(BOARD_WIDTH, BOARD_HEIGHT);
 
-    const flipped = [];
+    let flipped = [];
     for (let x = -1; x <= 1; x++) {
         for (let y = -1; y <= 1; y++) {
             if (x === 0 && y === 0) {
                 continue;
             }
 
+            let lineFlipped = [];
             // Start one square away from the target square
             for (let i = 0 + 1; i < maxDim; i++) {
 
-                const targetx = x * i;
-                const targety = y * i;
+                const targetx = pos.row + (x * i);
+                const targety = pos.col + (y * i);
                 if (
                     targetx === BOARD_WIDTH ||
                     targety === BOARD_HEIGHT ||
@@ -44,34 +45,84 @@ const calculateFlipped = (square, color) => {
                 }
 
                 const val = game.board[targetx][targety];
+                let breakLoop = false;
+                switch (val) {
+                    case enemyColor:
+                        // One of the enemies. Maybe we can keep it.
+                        lineFlipped.push(positionToCellID(targetx, targety));
+                        break;
+                    case playerColor:
+                        // Bookend by one of the player pieces. We DO
+                        // get to keep them.
+                        flipped = flipped.concat(lineFlipped);
+                        breakLoop = true;
+                        break;
+                    case CELL_EMPTY:
+                        breakLoop = true;
+                        break;
+                    default:
+                        throw new Error("Unknown cell type");
+                }
 
-
+                if (breakLoop) {
+                    break;
+                }
             }
-
-
         }
     }
-
+    return flipped;
 };
 
 const handleTurn = () => {
     const gamePlayer = game.players[game.player];
     const user = getUsername();
 
+    const statusEl = document.querySelector("#game-status");
+
+
     // Names in the HOT list can get chopped off, so we only check
     // for a starting match.
     if (user.indexOf(gamePlayer) !== 0) {
+        statusEl.innerHTML = `Waiting on: Player# ${game.player} ${gamePlayer}`;
         return;
     }
 
-    $('body').on('click', `.${CELL_CLASS}`, function () {
-        const cell = +this.dataset.cell;
-        const color = [CELL_WHITE, CELL_BLACK][game.player];
+    statusEl.innerHTML = `
+    <div> <b>It is your turn. Player ${game.player}</b></div>
+    <button id="skip-turn" type="button">SKIP TURN</button>
+    `;
 
-        playTurn(cell, color);
+    const advanceTurn = () => {
         game.player += 1;
         game.player = game.player % game.players.length;
         postGame();
+    };
+
+    $('body').on('click', `#skip-turn`, function () {
+        advanceTurn();
+    });
+
+    $('body').on('click', `.${CELL_CLASS}`, function () {
+        const cellID = +this.dataset.cell;
+        const playerColor = [CELL_WHITE, CELL_BLACK][game.player];
+        const enemyColor = [CELL_BLACK, CELL_WHITE][game.player];
+
+        const pos = cellIDToPosition(cellID);
+        if (game.board[pos.row][pos.col] !== CELL_EMPTY) {
+            alert("That position has already been played");
+            return;
+        }
+
+
+        const targets = calculateFlipped(cellID, playerColor, enemyColor);
+        const isTurnGood = targets.length !== 0;
+
+        if (isTurnGood) {
+            playTurn(cellID, playerColor, enemyColor);
+            advanceTurn();
+        } else {
+            alert("That position does not turn over any enemy pieces.");
+        }
     });
 };
 //////////////////////////////////////////////////////////////////////
@@ -86,14 +137,14 @@ const butcherName = (s) => {
     return s.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
 };
 
-const squareToPosition = (square) => {
-    const col = Math.floor(square % BOARD_WIDTH);
-    const row = Math.floor(square / BOARD_WIDTH);
+const cellIDToPosition = (cellID) => {
+    const col = Math.floor(cellID % BOARD_WIDTH);
+    const row = Math.floor(cellID / BOARD_WIDTH);
 
     return { row: row, col: col };
 }
 
-const posToSquare = (x, y) => {
+const positionToCellID = (x, y) => {
     return x * BOARD_WIDTH + y;
 };
 
@@ -116,27 +167,30 @@ const loadGame = () => {
     game.board = board;
     game.history = ""
 
-    console.log("*** HISTORY ***");
-    console.log(h, h.length);
+    // console.log("*** HISTORY ***");
+    // console.log(h, h.length);
     for (let i = 0; i < h.length; i += 2) {
-        const square = BYTE_ENCODE.indexOf(h[i]);
-        const player = h[i + 1];
-        console.log(square, player);
-        playTurn(square, player);
+        const cellID = BYTE_ENCODE.indexOf(h[i]);
+        const playerColor = h[i + 1];
+        const enemyColor = playerColor == CELL_WHITE ? CELL_BLACK : CELL_WHITE;
+
+        // console.log(cellID, playerColor, enemyColor);
+        playTurn(cellID, playerColor, enemyColor);
     }
 
-    console.log("Found save: ");
-    console.log(latestSave);
+    // console.log("Found save: ");
+    // console.log(latestSave);
 };
 
 const postGame = () => {
+    // The board is huge -- no reason to inflate the save file with it.
     delete (game.board);
 
     game.soiTime = +document.querySelector("[name='vqxti']").value;
 
     const str = btoa(JSON.stringify(game));
     const chatbox = document.querySelector("[name='vqxsp']");
-    chatbox.value = `<span class="${POST_CLASS}" style="display:none">${str}</span>Game Turn<br>${JSON.stringify(game, null, 2)}`;
+    chatbox.value = `<span class="${POST_CLASS}" style="display:none">${str}</span>Game Turn`;
 
     document.querySelector("[name='vqvaj']").click();
 };
@@ -162,15 +216,15 @@ const issueChallenge = () => {
         const victim = $(el.currentTarget).attr("data-name");
 
         for (let i = 0; i < 64; i++) {
-            setSquare(i, CELL_EMPTY);
+            setByCellID(i, CELL_EMPTY);
         }
 
         game.history = "";
 
-        playTurn(27, CELL_WHITE);
-        playTurn(28, CELL_BLACK);
-        playTurn(36, CELL_WHITE);
-        playTurn(35, CELL_BLACK);
+        playTurn(27, CELL_WHITE, CELL_BLACK);
+        playTurn(28, CELL_BLACK, CELL_WHITE);
+        playTurn(36, CELL_WHITE, CELL_BLACK);
+        playTurn(35, CELL_BLACK, CELL_WHITE);
 
         game.players = [butcherName(victim), getUsername()];
         game.player = 0;
@@ -181,7 +235,12 @@ const issueChallenge = () => {
 const initGame = () => {
     const $table = $("[name='vqxto']").closest("table");
     const $form = $("[name='vqxto']").closest("form");
-    $("<div id='gameboard'></div>").insertBefore($table);
+    $(`
+    <section>
+      <div id='gameboard'></div>
+      <div id='game-status'></div>
+    </section>
+     `).insertBefore($table);
     $form.append($(`
     <div id="gamepanel">
        <button type="button" id='start-game'>Start Game</button>
@@ -196,9 +255,9 @@ const initGame = () => {
         const rowArray = [];
 
         for (var w = 0; w < BOARD_WIDTH; w++) {
-            const cell = posToSquare(w, h);
+            const cellID = positionToCellID(w, h);
 
-            html += `<span data-cell=${cell} data-row=${h} data-col=${w} class='${CELL_CLASS}'><span></span></span>`;
+            html += `<span data-cell=${cellID} data-row=${h} data-col=${w} class='${CELL_CLASS}'><span></span></span>`;
             rowArray.push(CELL_EMPTY);
         }
 
@@ -217,10 +276,10 @@ const initGame = () => {
     }
 };
 
-const setSquare = (square, value) => {
-    console.log('setSquare', square, value);
+const setByCellID = (cellID, value) => {
+    console.log('setSquare', cellID, value);
 
-    const pos = squareToPosition(square);
+    const pos = cellIDToPosition(cellID);
 
     let className = CELL_EMPTY_CLASS;
     if (value === CELL_WHITE) {
@@ -228,20 +287,23 @@ const setSquare = (square, value) => {
     } else if (value === CELL_BLACK) {
         className = CELL_BLACK_CLASS;
     }
-    
-    console.log(pos.row, pos.col,  value);
+
+    console.log(pos.row, pos.col, value);
 
     game.board[pos.row][pos.col] = value;
-    const cell = $(`span[data-cell="${square}"] span`);
-    cell.removeClass([CELL_EMPTY_CLASS, CELL_WHITE_CLASS, CELL_BLACK_CLASS].join(" "));
-    cell.addClass(className);
+    const cellElement = $(`span[data-cell="${cellID}"] span`);
+    cellElement.removeClass([CELL_EMPTY_CLASS, CELL_WHITE_CLASS, CELL_BLACK_CLASS].join(" "));
+    cellElement.addClass(className);
 };
 
-const playTurn = (square, value) => {
-    const byte = BYTE_ENCODE[square];
-    game.history += (byte + value);
+const playTurn = (cellID, playerColor, enemyColor) => {
+    const byte = BYTE_ENCODE[cellID];
+    game.history += (byte + playerColor);
 
-    setSquare(square, value);
+    const targets = calculateFlipped(cellID, playerColor, enemyColor);
+
+    targets.forEach(cellID => setByCellID(cellID, playerColor));
+    setByCellID(cellID, playerColor);
 };
 
 let game = initGame();
