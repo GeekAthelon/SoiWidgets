a68a522803bf43b8ab51c831b8f0f707be5e4988
Geek Athelon <geekathelon@gmail.com>
2020-06-04 01:31:00 -0400

    Fixed stuff

--- rooms/flipit/flipit.js
+++ rooms/flipit/flipit.js
@@ -2,6 +2,8 @@
 
 const BOARD_WIDTH = 8;
 const BOARD_HEIGHT = 8;
+const BOARD_LEN = BOARD_WIDTH * BOARD_HEIGHT;
+
 const $ = jQuery;
 
 // Encode our square numbers (0..63) as one of these characters.
@@ -18,12 +20,21 @@ const CELL_BLACK_CLASS = 'cell_black';
 
 const POST_CLASS = "save-game";
 const CELL_CLASS = "flipit-cell";
+const RECENT_CELL_CLASS = 'flipit-recent';
+const HINT_CELL_CLASS = 'cell_hint';
 
 const calculateFlipped = (cellID, playerColor, enemyColor) => {
+    const cellContents = getByCellID(cellID);
+    if (cellContents !== CELL_EMPTY) {
+        if (cellID === 62) debugger;
+        return [];
+    }
+
     const pos = cellIDToPosition(cellID);
     const maxDim = Math.max(BOARD_WIDTH, BOARD_HEIGHT);
 
     let flipped = [];
+
     for (let x = -1; x <= 1; x++) {
         for (let y = -1; y <= 1; y++) {
             if (x === 0 && y === 0) {
@@ -73,61 +84,167 @@ const calculateFlipped = (cellID, playerColor, enemyColor) => {
     return flipped;
 };
 
+const getHint = (playerColor, enemyColor) => {
+    const validMoves = [];
+    for (let cellID = 0; cellID < BOARD_LEN; cellID++) {
+        const value = getByCellID(cellID);
+        if (value !== CELL_EMPTY) {
+            continue;
+        }
+
+        const toFlip = calculateFlipped(cellID, playerColor, enemyColor);
+
+        if (toFlip.length > 0) {
+            validMoves.push([cellID, toFlip]);
+        }
+    }
+
+    const delay = 5 * 1000;
+
+    let idx = 0;
+
+    const showHint = () => {
+        var item = validMoves[idx];
+        if (!item) {
+            return;
+        }
+
+        const cellID = item[0];
+        const targets = item[1];
+
+        targets.forEach(cellID => $(`span[data-cell="${cellID}"] span`).addClass(HINT_CELL_CLASS));
+        $(`span[data-cell="${cellID}"] span`).addClass(HINT_CELL_CLASS);
+
+        setTimeout(() => {
+            $(`.${HINT_CELL_CLASS}`).removeClass(HINT_CELL_CLASS);
+            idx += 1;
+            showHint();
+        }, delay);
+    }
+    showHint();
+};
+
 const handleTurn = () => {
     const gamePlayer = game.players[game.player];
     const user = getUsername();
 
+    const playerColor = [CELL_WHITE, CELL_BLACK][game.player];
+    const enemyColor = [CELL_BLACK, CELL_WHITE][game.player];
+
     const statusEl = document.querySelector("#game-status");
 
+    const counts = {};
+    [CELL_WHITE, CELL_BLACK, CELL_EMPTY].forEach(t => counts[t] = 0);
+
+    const moves = {};
+    [CELL_WHITE, CELL_BLACK, CELL_EMPTY].forEach(t => moves[t] = 0);
+
+    for (let cellID = 0; cellID < BOARD_LEN; cellID++) {
+        const value = getByCellID(cellID);
+        counts[value] += 1;
+
+        // Scan the whole board and see if there are moves that can be made
+        // by either side.
+
+        const playerFlips = calculateFlipped(cellID, playerColor, enemyColor);
+        if (playerFlips.length > 0) {
+            moves[playerColor] += 1;
+        }
+
+        const enemyFlips = calculateFlipped(cellID, enemyColor, playerColor);
+        if (enemyFlips.length > 0) {
+            moves[enemyColor] += 1;
+        }
+    }
+
+    statusEl.innerHTML = `<b>White:</b> ${counts[CELL_WHITE]} <b>Black:</b> ${counts[CELL_BLACK]}<br>`;
+
+    if (moves[playerColor] === 0 && moves[enemyColor] === 0) {
+        statusEl.innerHTML += "Neither play can make a turn. Game over.<br>";
+        return;
+    }
+
+    let skipMode = " disabled='disabled' ";
+    if (moves[playerColor] === 0) {
+        statusEl.innerHTML += "Player cannot make a turn. Some would skip. Some would end the game.<br>";
+        skipMode  = "";
+    }
 
     // Names in the HOT list can get chopped off, so we only check
     // for a starting match.
     if (user.indexOf(gamePlayer) !== 0) {
-        statusEl.innerHTML = `Waiting on: Player# ${game.player} ${gamePlayer}`;
+        statusEl.innerHTML += `Waiting on: Player# ${game.player} ${gamePlayer}`;
         return;
     }
 
-    statusEl.innerHTML = `
-    <div> <b>It is your turn. Player ${game.player}</b></div>
-    <button id="skip-turn" type="button">SKIP TURN</button>
+    if (game.hint) {
+        getHint(playerColor, enemyColor);
+        delete game.hint;
+    }
+
+    const colorName = ["white", "black"][game.player];
+
+    statusEl.innerHTML += `
+    <div> <b>It is your turn. Player ${game.player} ${colorName}</b></div>
+    <div class="error-message"></div>
+    <button id="skip-turn" type="button" ${skipMode}>SKIP TURN</button>
+    <button id="get-hint" type="button">Show all possible moves (HINT)</button>
     `;
 
-    const advanceTurn = () => {
+    const advanceTurn = (msg) => {
         game.player += 1;
         game.player = game.player % game.players.length;
-        postGame();
+        postGame(msg);
     };
 
     $('body').on('click', `#skip-turn`, function () {
-        advanceTurn();
+        advanceTurn('Skipped turn');
     });
+
+    $('body').on('click', `#get-hint`, function () {
+        game.hint = true;
+        postGame('Requested a hint');
+    });
+
 
     $('body').on('click', `.${CELL_CLASS}`, function () {
         const cellID = +this.dataset.cell;
-        const playerColor = [CELL_WHITE, CELL_BLACK][game.player];
-        const enemyColor = [CELL_BLACK, CELL_WHITE][game.player];
+        showError("");
 
-        const pos = cellIDToPosition(cellID);
-        if (game.board[pos.row][pos.col] !== CELL_EMPTY) {
-            alert("That position has already been played");
+        const value = getByCellID(cellID);
+        if (value !== CELL_EMPTY) {
+            showError("That position has already been played");
             return;
         }
-
 
         const targets = calculateFlipped(cellID, playerColor, enemyColor);
         const isTurnGood = targets.length !== 0;
 
         if (isTurnGood) {
             playTurn(cellID, playerColor, enemyColor);
-            advanceTurn();
+            advanceTurn(`Played in square ${cellID}`);
         } else {
-            alert("That position does not turn over any enemy pieces.");
+            showError("That position does not turn over any enemy pieces.");
         }
     });
 };
+
 //////////////////////////////////////////////////////////////////////
 /// UTILS
 //////////////////////////////////////////////////////////////////////
+
+const showError = (msg) => {
+    const errorEl = document.querySelector(".error-message");
+
+    if (msg === "") {
+        errorEl.style.display = "none";
+    } else {
+        errorEl.style.display = "block";
+        errorEl.innerHTML = msg;
+        errorEl.scrollIntoView(false);
+    }
+};
+
 const getUsername = () => {
     const s = document.getElementsByName("vqxha")[0].value;
     return butcherName(s);
@@ -146,6 +263,10 @@ const cellIDToPosition = (cellID) => {
 
 const positionToCellID = (x, y) => {
     return x * BOARD_WIDTH + y;
+};
+
+const clearClasses = (el) => {
+    el.removeClass([CELL_EMPTY_CLASS, CELL_WHITE_CLASS, CELL_BLACK_CLASS].join(" "));
 };
 
 
@@ -167,22 +288,16 @@ const loadGame = () => {
     game.board = board;
     game.history = ""
 
-    // console.log("*** HISTORY ***");
-    // console.log(h, h.length);
     for (let i = 0; i < h.length; i += 2) {
         const cellID = BYTE_ENCODE.indexOf(h[i]);
         const playerColor = h[i + 1];
         const enemyColor = playerColor == CELL_WHITE ? CELL_BLACK : CELL_WHITE;
 
-        // console.log(cellID, playerColor, enemyColor);
         playTurn(cellID, playerColor, enemyColor);
     }
-
-    // console.log("Found save: ");
-    // console.log(latestSave);
 };
 
-const postGame = () => {
+const postGame = (msg) => {
     // The board is huge -- no reason to inflate the save file with it.
     delete (game.board);
 
@@ -190,7 +305,7 @@ const postGame = () => {
 
     const str = btoa(JSON.stringify(game));
     const chatbox = document.querySelector("[name='vqxsp']");
-    chatbox.value = `<span class="${POST_CLASS}" style="display:none">${str}</span>Game Turn`;
+    chatbox.value = chatbox.value + `<span class="${POST_CLASS}" style="display:none">${str}</span>${msg}`;
 
     document.querySelector("[name='vqvaj']").click();
 };
@@ -215,8 +330,8 @@ const issueChallenge = () => {
     $("[data-id='challenge']").click((el) => {
         const victim = $(el.currentTarget).attr("data-name");
 
-        for (let i = 0; i < 64; i++) {
-            setByCellID(i, CELL_EMPTY);
+        for (let cellID = 0; cellID < BOARD_LEN; cellID++) {
+            setByCellID(cellID, CELL_EMPTY);
         }
 
         game.history = "";
@@ -228,7 +343,7 @@ const issueChallenge = () => {
 
         game.players = [butcherName(victim), getUsername()];
         game.player = 0;
-        postGame();
+        postGame('Started a new game');
     });
 };
 
@@ -276,9 +391,12 @@ const initGame = () => {
     }
 };
 
-const setByCellID = (cellID, value) => {
-    console.log('setSquare', cellID, value);
+const getByCellID = (cellID) => {
+    const pos = cellIDToPosition(cellID);
+    return game.board[pos.row][pos.col];
+}
 
+const setByCellID = (cellID, value) => {
     const pos = cellIDToPosition(cellID);
 
     let className = CELL_EMPTY_CLASS;
@@ -288,11 +406,9 @@ const setByCellID = (cellID, value) => {
         className = CELL_BLACK_CLASS;
     }
 
-    console.log(pos.row, pos.col, value);
-
     game.board[pos.row][pos.col] = value;
     const cellElement = $(`span[data-cell="${cellID}"] span`);
-    cellElement.removeClass([CELL_EMPTY_CLASS, CELL_WHITE_CLASS, CELL_BLACK_CLASS].join(" "));
+    clearClasses(cellElement);
     cellElement.addClass(className);
 };
 
@@ -302,8 +418,13 @@ const playTurn = (cellID, playerColor, enemyColor) => {
 
     const targets = calculateFlipped(cellID, playerColor, enemyColor);
 
+    $(`.${CELL_CLASS}`).removeClass(RECENT_CELL_CLASS);
+
     targets.forEach(cellID => setByCellID(cellID, playerColor));
+    targets.forEach(cellID => $(`span[data-cell="${cellID}"]`).addClass(RECENT_CELL_CLASS));
+
     setByCellID(cellID, playerColor);
+    $(`span[data-cell="${cellID}"]`).addClass(RECENT_CELL_CLASS);
 };
 
 let game = initGame();
